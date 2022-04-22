c53b49daf13ec8dbf822a80eef67c09e6cc3534e
Aleks Pawlak <aleks.pawlak@code.berlin>
2022-04-22 11:24:36 +0200

    second commit

---
+++ main.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request, redirect
+from flask.helpers import url_for
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/contact', methods=["POST", "GET"])
+def contact():
+    if request.method == "POST":
+        email = request.form["em"]
+        return redirect(url_for("email", eml=email))
+    else:
+        return render_template('contact.html')
+
+
+@app.route("/<eml>")
+def email(eml):
+    return f"<h1>The answer will be send to the following email:{eml}</h1>"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
