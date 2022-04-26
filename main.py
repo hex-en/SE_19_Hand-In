e15d9d69379a1fca254b54c8037f6ac9bd935e78
Aleks Pawlak <aleks.pawlak@code.berlin>
2022-04-26 14:07:30 +0200

    Added contact form with db support

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
+from email import message
 from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
 from flask.helpers import url_for
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///inquiries.db'
+
+db = SQLAlchemy(app)
+
+
+class Inquiries(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(50), nullable=False)
+    message = db.Column(db.String(200), nullable=True)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return'<Message %r>' % self.id
 
 
 @app.route('/')
@@ -16,9 +33,21 @@ def reviews():
 
 @app.route('/contact', methods=["POST", "GET"])
 def contact():
+
     if request.method == "POST":
-        email = request.form["em"]
-        return redirect(url_for("email", eml=email))
+        username = request.form['name']
+        mail_adress = request.form['email']
+        user_inquirie = request.form['message']
+
+        new_inquirie = Inquiries(
+            name=username, email=mail_adress, message=user_inquirie)
+
+        try:
+            db.session.add(new_inquirie)
+            db.session.commit()
+            return redirect('/')
+        except Exception:
+            return "There wass an error sending your inquirie"
     else:
         return render_template('contact.html')
 
